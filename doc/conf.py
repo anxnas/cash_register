63765c08e584354a2c45ace3a2f688a782106619
anxnas <106204348+anxnas@users.noreply.github.com>
2024-08-17 19:23:21 +0300

    Set up GitHub Actions for security analysis and documentation

--- doc/conf.py
+++ doc/conf.py
@@ -6,8 +6,8 @@
 # -- Path setup ------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./shop'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../shop'))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cash_register.settings'
 
